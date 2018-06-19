e9486b09d6794d56d11281eb0f1e601b2a5dc7f9
Casper Groenenberg <c.groenenberg@hotmail.com>
2018-06-19 18:12:26 +0200

    Working mutation

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const express_graphql = require("express-graphql");
 const { buildSchema } = require("graphql");
 const config = require("./config/config");
+const db = require("./db");
 const app = express();
 
 //GraphQL schema
@@ -10,17 +11,60 @@ const schema = buildSchema(`
         course(id: Int!): Course
         courses(topic: String): [Course]
     }
+    type Mutation {
+        updateCourseTopic(id: Int!, topic: String!): Course
+    }
     type Course {
         id: Int,
         title: String,
         author: String,
-        topic: String
+        description: String,
+        topic: String,
+        url: String
     }
 `);
 
+
+const getCourse = (args) => {
+    let id = args.id;
+    return db.filter(course => {
+        return course.id == id;
+    })[0]
+};
+
+const getCourses = (args) => {
+    if (args.topic) {
+        let topic = args.topic;
+        return db.filter(course => {
+            return course.topic === topic
+        })
+    } else {
+        return db;
+    }
+};
+
+const updateCourseTopic = ({id, topic}) => {
+    if (id && topic) {
+        db.map(course => {
+            if (course.id === id) {
+                course.topic = topic;
+                return course;
+            }
+        });
+    } else {
+        return db;
+    }
+    return db.filter(course => {
+        return course.id == id;
+    })[0];
+};
+
 //Root resolver
 const root = {
-    message: () => 'Hello world'
+    course: getCourse,
+    courses: getCourses,
+    updateCourseTopic: updateCourseTopic
+
 };
 
 
